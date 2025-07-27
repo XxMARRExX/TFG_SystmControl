3dc826f26ee0ed2cb4fca27ea2a4d03227d4e10b
Cristian Marrero <cristian.marrero104@alu.ulpgc.es>
2025-07-27 17:03:31 +0100

    Cambios con respecto a la capa: 15 -- Superposición de ambos Bboxes --

--- src/lace/laceVisualization/drawBoundingBoxesAlignment.m
+++ src/lace/laceVisualization/drawBoundingBoxesAlignment.m
@@ -1,20 +1,24 @@
-function drawBoundingBoxesAlignment(cornersSVG, cornersPiezaAligned)
-% DRAWBOUNDINGBOXALIGNMENT Dibuja el encaje entre el BBox del SVG y el BBox alineado de la pieza
+function drawBoundingBoxesAlignment(cornersSVG, cornersPieceAligned)
+% DRAWBOUNDINGBOXESALIGNMENT Visualizes the alignment between the SVG and the detected piece bounding boxes.
 %
-% Entrada:
-%   - cornersSVG:          4x2 matriz, esquinas del bounding box del modelo SVG
-%   - cornersPiezaAligned: 4x2 matriz, esquinas del bounding box alineado de la pieza (salida Z)
+% Inputs:
+%   - cornersSVG:          4x2 matrix, corners of the SVG model bounding box
+%   - cornersPiezaAligned: 4x2 matrix, corners of the aligned piece bounding box (after Procrustes)
 
+    % 1. Prepare figure
     figure; hold on; axis equal;
-    title("Encaje de BoundingBoxes: SVG (verde) vs Pieza (rojo)");
+    title("Bounding Box Alignment: SVG (green) vs Piece (red)");
 
-    % Dibujar BBox del SVG
-    loopSVG = [cornersSVG; cornersSVG(1,:)];  % cerrar lazo
-    plot(loopSVG(:,1), loopSVG(:,2), 'g-', 'LineWidth', 2);
+    % 2. Draw SVG bounding box
+    loopSVG = [cornersSVG; cornersSVG(1,:)];  % close the loop
+    plot(loopSVG(:,1), loopSVG(:,2), 'g-', 'LineWidth', 2, ...
+        'DisplayName', 'BBox SVG');
 
-    % Dibujar BBox de la pieza alineada
-    loopP = [cornersPiezaAligned; cornersPiezaAligned(1,:)];
-    plot(loopP(:,1), loopP(:,2), 'r--', 'LineWidth', 2);
+    % 3. Draw aligned piece bounding box
+    loopP = [cornersPieceAligned; cornersPieceAligned(1,:)];
+    plot(loopP(:,1), loopP(:,2), 'r--', 'LineWidth', 2, ...
+        'DisplayName', 'BBox Piece (aligned)');
 
-    legend({'BBox SVG','BBox Pieza Alineada'}, 'Location', 'best');
+    % 4. Finalize
+    legend('show', 'Location', 'best');
 end
