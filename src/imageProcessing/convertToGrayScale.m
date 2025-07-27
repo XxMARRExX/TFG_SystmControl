c0007838f372c9d6f9a1ef26f618d59708d3aeda
Cristian Marrero <cristian.marrero104@alu.ulpgc.es>
2025-07-27 09:42:55 +0100

    Cambios con respecto a la capa: 1 -- Paso de la imagen a gris --

--- src/imageProcessing/convertToGrayScale.m
+++ src/imageProcessing/convertToGrayScale.m
@@ -1,8 +1,17 @@
-function img_gray = convertToGrayScale(img)
-    % Convierte una imagen a escala de grises
-    if size(img, 3) == 3
-        img_gray = rgb2gray(img);
+function grayImage = convertToGrayScale(image)
+%CONVERTTOGRAYSCALE Converts an image to grayscale if it is RGB.
+%
+%   Input:
+%       image - Input image (RGB or grayscale).
+%
+%   Output:
+%       grayImage - Image in grayscale format (uint8 or double, same as input).
+
+    if ndims(image) == 3 && size(image, 3) == 3
+        grayImage = rgb2gray(image);
+    elseif ndims(image) == 2
+        grayImage = image;
     else
-        img_gray = img;
+        error('Unsupported image format: must be RGB or grayscale 2D image.');
     end
 end
