bf6891e1dc197ae9383fa700fe972e47b5b1bcfe
Cristian Marrero <cristian.marrero104@alu.ulpgc.es>
2025-07-27 09:53:05 +0100

    Cambios con respecto a la capa: 4 -- Cálculo del Bbox de la pieza --

--- src/Piece/BboxPiece/calculateExpandedBoundingBox.m
+++ src/Piece/BboxPiece/calculateExpandedBoundingBox.m
@@ -1,6 +1,18 @@
 function bBoxExpanded = calculateExpandedBoundingBox(edges, scale, margin)
+%CALCULATEEXPANDEDBOUNDINGBOX Computes the expanded bounding box of a set of edges.
+%
+%   Inputs:
+%       edges - Struct with fields 'x' and 'y' (e.g., from subpixelEdges).
+%       scale - Rescaling factor applied to the image prior to edge detection.
+%       margin - Margin (in pixels or units) to expand the bounding box.
+%
+%   Output:
+%       bBoxExpanded - 2xN matrix with coordinates of the expanded bounding box.
+
     points = [edges.x, edges.y];
-    bBoxPiece = minBoundingBox(points');
-    bBoxPieceRescaled = bBoxPiece / scale;
-    bBoxExpanded = expandBoundingBox(bBoxPieceRescaled, margin);
+
+    bBoxRescaled = minBoundingBox(points');
+    bBoxOriginal = bBoxRescaled / scale;
+
+    bBoxExpanded = expandBoundingBox(bBoxOriginal, margin);
 end
